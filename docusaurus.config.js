b1697921660405e07967a50cc0ee39981250b334
Jaswinder <jaswinder.singh@iitdalumni.com>
2024-01-08 16:44:43 +0530

    minor changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/JaswinderSingh22/Documentation/tree/main",
         },
         blog: {
           showReadingTime: true,
@@ -100,7 +100,7 @@ const config = {
             items: [
               {
                 label: "Tutorial",
-                to: "/info/intro",
+                to: "/docs/intro",
               },
             ],
           },
